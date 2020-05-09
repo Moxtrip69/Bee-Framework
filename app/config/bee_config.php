efad3aa41df3239ddb7d473867f73b30e65d4c8e
Moxtrip69 <roborozco@joystick.com.mx>
2020-05-08 23:57:04 -0500

    Updated

--- app/config/bee_config.php
+++ app/config/bee_config.php
@@ -49,7 +49,7 @@ define('UPLOADS'    , ASSETS.'uploads/');
 // Set para conexión local o de desarrollo
 define('LDB_ENGINE' , 'mysql');
 define('LDB_HOST'   , 'localhost');
-define('LDB_NAME'   , 'u5_p1_db');
+define('LDB_NAME'   , '__LOCAL_DB__');
 define('LDB_USER'   , 'root');
 define('LDB_PASS'   , '');
 define('LDB_CHARSET', 'utf8');
