d8c3f7c37f9046975123d93e00b00dd1e792ca2a
Roberto Orozco <roborozco@joystick.com.mx>
2021-12-11 16:37:16 -0600

    Nueva update
    Mejoras a los componentes de vue, nuevas funciones siendo implementadas y en revisión.

--- app/functions/bee_custom_functions.php
+++ app/functions/bee_custom_functions.php
@@ -1,2 +1,163 @@
 <?php 
 
+/**
+ * Verifies that an email is valid.
+ *
+ * Does not grok i18n domains. Not RFC compliant.
+ *
+ * @since 0.71
+ *
+ * @param string $email      Email address to verify.
+ * @param bool   $deprecated Deprecated.
+ * @return string|false Valid email address on success, false on failure.
+ */
+function is_email( $email, $deprecated = false ) {
+	if ( ! empty( $deprecated ) ) {
+		_deprecated_argument( __FUNCTION__, '3.0.0' );
+	}
+
+	// Test for the minimum length the email can be.
+	if ( strlen( $email ) < 6 ) {
+		/**
+		 * Filters whether an email address is valid.
+		 *
+		 * This filter is evaluated under several different contexts, such as 'email_too_short',
+		 * 'email_no_at', 'local_invalid_chars', 'domain_period_sequence', 'domain_period_limits',
+		 * 'domain_no_periods', 'sub_hyphen_limits', 'sub_invalid_chars', or no specific context.
+		 *
+		 * @since 2.8.0
+		 *
+		 * @param string|false $is_email The email address if successfully passed the is_email() checks, false otherwise.
+		 * @param string       $email    The email address being checked.
+		 * @param string       $context  Context under which the email was tested.
+		 */
+		return apply_filters( 'is_email', false, $email, 'email_too_short' );
+	}
+
+	// Test for an @ character after the first position.
+	if ( strpos( $email, '@', 1 ) === false ) {
+		/** This filter is documented in wp-includes/formatting.php */
+		return apply_filters( 'is_email', false, $email, 'email_no_at' );
+	}
+
+	// Split out the local and domain parts.
+	list( $local, $domain ) = explode( '@', $email, 2 );
+
+	// LOCAL PART
+	// Test for invalid characters.
+	if ( ! preg_match( '/^[a-zA-Z0-9!#$%&\'*+\/=?^_`{|}~\.-]+$/', $local ) ) {
+		/** This filter is documented in wp-includes/formatting.php */
+		return apply_filters( 'is_email', false, $email, 'local_invalid_chars' );
+	}
+
+	// DOMAIN PART
+	// Test for sequences of periods.
+	if ( preg_match( '/\.{2,}/', $domain ) ) {
+		/** This filter is documented in wp-includes/formatting.php */
+		return apply_filters( 'is_email', false, $email, 'domain_period_sequence' );
+	}
+
+	// Test for leading and trailing periods and whitespace.
+	if ( trim( $domain, " \t\n\r\0\x0B." ) !== $domain ) {
+		/** This filter is documented in wp-includes/formatting.php */
+		return apply_filters( 'is_email', false, $email, 'domain_period_limits' );
+	}
+
+	// Split the domain into subs.
+	$subs = explode( '.', $domain );
+
+	// Assume the domain will have at least two subs.
+	if ( 2 > count( $subs ) ) {
+		/** This filter is documented in wp-includes/formatting.php */
+		return apply_filters( 'is_email', false, $email, 'domain_no_periods' );
+	}
+
+	// Loop through each sub.
+	foreach ( $subs as $sub ) {
+		// Test for leading and trailing hyphens and whitespace.
+		if ( trim( $sub, " \t\n\r\0\x0B-" ) !== $sub ) {
+			/** This filter is documented in wp-includes/formatting.php */
+			return apply_filters( 'is_email', false, $email, 'sub_hyphen_limits' );
+		}
+
+		// Test for invalid characters.
+		if ( ! preg_match( '/^[a-z0-9-]+$/i', $sub ) ) {
+			/** This filter is documented in wp-includes/formatting.php */
+			return apply_filters( 'is_email', false, $email, 'sub_invalid_chars' );
+		}
+	}
+
+	// Congratulations, your email made it!
+	/** This filter is documented in wp-includes/formatting.php */
+	return apply_filters( 'is_email', $email, $email, null );
+}
+
+/**
+ * Calls the callback functions that have been added to a filter hook.
+ *
+ * The callback functions attached to the filter hook are invoked by calling
+ * this function. This function can be used to create a new filter hook by
+ * simply calling this function with the name of the new hook specified using
+ * the `$tag` parameter.
+ *
+ * The function also allows for multiple additional arguments to be passed to hooks.
+ *
+ * Example usage:
+ *
+ *     // The filter callback function.
+ *     function example_callback( $string, $arg1, $arg2 ) {
+ *         // (maybe) modify $string.
+ *         return $string;
+ *     }
+ *     add_filter( 'example_filter', 'example_callback', 10, 3 );
+ *
+ *     /*
+ *      * Apply the filters by calling the 'example_callback()' function
+ *      * that's hooked onto `example_filter` above.
+ *      *
+ *      * - 'example_filter' is the filter hook.
+ *      * - 'filter me' is the value being filtered.
+ *      * - $arg1 and $arg2 are the additional arguments passed to the callback.
+ *     $value = apply_filters( 'example_filter', 'filter me', $arg1, $arg2 );
+ *
+ * @since 0.71
+ *
+ * @global WP_Hook[] $wp_filter         Stores all of the filters and actions.
+ * @global string[]  $wp_current_filter Stores the list of current filters with the current one last.
+ *
+ * @param string $tag     The name of the filter hook.
+ * @param mixed  $value   The value to filter.
+ * @param mixed  ...$args Additional parameters to pass to the callback functions.
+ * @return mixed The filtered value after all hooked functions are applied to it.
+ */
+function apply_filters( $tag, $value ) {
+	global $wp_filter, $wp_current_filter;
+
+	$args = func_get_args();
+
+	// Do 'all' actions first.
+	if ( isset( $wp_filter['all'] ) ) {
+		$wp_current_filter[] = $tag;
+		_wp_call_all_hook( $args );
+	}
+
+	if ( ! isset( $wp_filter[ $tag ] ) ) {
+		if ( isset( $wp_filter['all'] ) ) {
+			array_pop( $wp_current_filter );
+		}
+		return $value;
+	}
+
+	if ( ! isset( $wp_filter['all'] ) ) {
+		$wp_current_filter[] = $tag;
+	}
+
+	// Don't pass the tag name to WP_Hook.
+	array_shift( $args );
+
+	$filtered = $wp_filter[ $tag ]->apply_filters( $value, $args );
+
+	array_pop( $wp_current_filter );
+
+	return $filtered;
+}
--- assets/js/components/testComponent.js
+++ assets/js/components/testComponent.js
@@ -1,17 +1,61 @@
 export const testComponent = {
   data() {
     return {
+      postId: null,
+      post: false,
     }
   },
+  created() {
+    this.get();
+  },
   methods: {
+    get() {
+      this.postId = Bee.current_params[0];
 
+      const body = {
+        id: this.postId
+      }
+
+      $.ajax({
+        url: 'ajax/test_get_post',
+        type: 'get',
+        dataTpe: 'json',
+        cache: false,
+        data: body
+      }).done(res => {
+        if (res.status === 200) {
+          this.post = res.data;
+        } else {
+          toastr.error(res.msg);
+        }
+      }).fail(err => {
+        toastr.error(err.responseJSON.msg);
+      }).always(() => {
+      })
+    }
   },
   template:
   /*html*/
   `
-  <div className="text-center p-5 bg-light border rounded shadow mt-5">
-    <p>Componente <code>testComponent.js</code> cargado con éxito</p>
-    <a href="vuejs" className="text-muted">Regresar</a>
+  <div className="p-5 bg-light border rounded shadow mt-5">
+    <div className="text-center">
+      <p>Componente <code>testComponent.js</code> cargado con éxito</p>
+      <a href="vuejs" className="text-muted">Regresar</a>
+    </div>
+
+    <div className="row mt-3" v-if="post">
+      <div className="col-12">
+        <div className="card">
+          <div className="card-header"><h4>{{post.titulo}}</h4></div>
+          <div className="card-body nl2br">
+            {{post.contenido}}
+          </div>
+          <div className="card-footer">
+            Por <span className="text-muted">{{post.nombre}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   `
 }
