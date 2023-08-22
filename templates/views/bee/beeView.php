874551b277156eaeeb06cd4a00c7be30266fcb97
Robert Orozco <roborozco@joystick.com.mx>
2023-08-22 08:29:15 -0600

    Update beeView.php

--- templates/views/bee/beeView.php
+++ templates/views/bee/beeView.php
@@ -94,7 +94,7 @@
         <div class="card-body" id="documentacion">
           <h2>Sencillo de usar</h2>
 
-          <h4>Instalación</h4>
+          <h4 id="instalacion"># Instalación</h4>
           <p>Para ejecutar <b>Bee framework</b> por primera vez basta con editar el archivo <code>bee_config.php</code> dentro de la carpeta <code>config</code> y el archivo <code>settings.php</code> dentro de <code>core</code>.</p>
           <p>Lo único que debes ajustar esencialmente es la ruta base o la constante <code>BASEPATH</code> del archivo <code>bee_config.php</code>, esta constante se encarga de indicar cuál es la ruta base o ruta principal del proyecto, tanto en desarrollo como en producción, por ejemplo: si tu proyecto se encuentra dentro de <code>htdocs/miempresa/</code> tu <code>BASEPATH</code> deberá usar <code>/miempresa/</code> como ruta en desarrollo.</p>
           <p>Normalmente cuando no cargan bien los estilos o imágenes del sitio al abrir por primera vez la página, es porque no se ha configurado de forma correcta la ruta base del framework.</p>
@@ -103,7 +103,7 @@
           <p>La constante <code>IS_LOCAL</code> simplementa hace una validación para verificar si nos encontramos en un entorno de desarrollo, si es así se usará el <code>BASEPATH</code> con su valor de desarrollo de lo contrario el de producción, en estos momentos estas son tus configuraciones:</p>
           <?php echo code_block('BASEPATH = ' . get_basepath()); ?>
 
-          <h4>Prepros</h4>
+          <h4 id="prepros"># Prepros</h4>
           <p>Si has visto alguno de mis cursos, te has dado cuenta que en mi stack de herramientas está <b><a href="https://prepros.io/" target="_blank">Prepros</a></b>, una herramienta gratuita que me ayuda a agilizar mi trabajo de forma muy sencilla, no es obligatorio, pero <b>Bee framework</b> puede trabajar en conjunto con él, sólo basta con editar la constante <code>PREPROS</code> en el archivo <code>settings.php</code>, esta acepta un valor <code>Booleano</code> que puede ser <code>true</code> si usarás Prepros, o <code>false</code> si no lo usarás, lo único que hace es concatenar en tu URL base el <b>puerto</b> en el que Prepros escucha cambios y actualizaciones del código para hacer su trabajo. También recuerda actualizar la constante <code>PORT</code> para indicar cuál es el puerto que usa Prepros para trabajar (aparece en tu dashboard del proyecto), regularmente es el puerto <code>8848</code>:</p>
           <?php echo code_block(
 "// Prepros 2023
@@ -113,7 +113,7 @@ define('PORT'    , '8848');") ?>
           <p>Lo cual genera la siguiente URL base para tus enlaces y assets (revisa que sea correcta, de lo contrario ajusta tu configuración):</p>
           <?php echo code_block('URL = ' . get_base_url()); ?>
           
-          <h4>Base de datos</h4>
+          <h4 id="db"># Base de datos</h4>
           <p>Dentro de los archivos del framework encontrarás el archivo <code>db_beeframework.sql</code>, este contiene el código SQL para la estructura y creación de las diferentes tablas requeridas por Bee framework y sus funciones básicas, tú puedes añadir o modificar a tu gusto en cualquier momento, asegúrate de actualizar las credenciales y nombre de tu base de datos como se indica en el siguiente bloque.</p>
 
           <p>En el archivo <code>settings.php</code> deberás configurar las credenciales de la base de datos local o de desarrollo:</p>
@@ -143,13 +143,13 @@ define('DEFAULT_CONTROLLER'       , 'bee');
 define('DEFAULT_ERROR_CONTROLLER' , 'error');
 define('DEFAULT_METHOD'           , 'index');"); ?>
 
-          <h4>Routing</h4>
+          <h4 id="routing"># Routing</h4>
           <p><b>Bee framework</b> está diseñado en el patrón MVC (Modelo Vista Controlar) lo que separa en capas diferentes la lógica, la información y la interfaz, manteniendo cada área aislada y enfocada en su responsabilidad.</p>
 
           <p>Para crear nuevas rutas basta con crear <code>controladores</code>, puedes hacerlo manualmente o usando la herramienta incluida llamada <code>Creator</code> puedes acceder a ella desde <a href="creator" target="_blank">aquí</a>, es decir si quieres una ruta por ejemplo para productos, basta con crear el controlador <code>productosController</code>, este debe ir dentro de la carpeta de controladores en <code>app/controllers</code>, cada nuevo controlador debe contar con mínimo un método llamado <code>index</code>, este será ejecutado por defecto al acceder a la ruta <code>productos</code>, si quisieras una ruta <code>productos/agregar</code>, basta con crear el método <code>agregar</code> dentro de <code>productosController</code>.</p>
           <?php echo code_block('<?php class productosController extends Controller {...} ?>'); ?>
 
-          <h4>Modelos</h4>
+          <h4 id="modelos"># Modelos</h4>
           <p>Al igual que los controladores, puedes crear nuevos modelos con la herramienta <code>Creator</code>, recuerda usar las convenciones para nombrar tus modelos para mantener un estándar en tu código y estructura, por ejemplo para crear un modelo que se encargue de manipular registros de <b>productos</b>, puedes crear el modelo <code>productModel</code> o <code>productoModel</code>, en singular, esto te dará acceso a los métodos generales para manipular información de la base de datos:</p>
           <?php echo code_block(file_get_contents(MODELS . 'productModel.php')); ?>
 
